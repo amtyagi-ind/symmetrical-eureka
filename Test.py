59bc73b47230dc1a5c0f3c6096a4f6158541694f
amtyagi-ind <amtyagi@yahoo.com>
2026-06-13 18:16:41 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,22 @@
+def factorial(j):
+    i=j;
+    j=1;
+    while i>1:
+        j=j*i
+        i=i-1        
+    return (j)
+
+fact= lambda x,y,z:(x*y*z)
+
 
 a='Hello World'
 b="Also"
-print(a+" "+b);
+d,e,f=1,2,3
+print(a+" "+b)
 
-print(b);
+print(b)
+j=factorial(10)
 
-print(1+2);
+print (j)
+print(d)
+print (fact(3,2,5))
