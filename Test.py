d18d10d50bfa790a6dd7f28c1fbfc96ecf10f5e5
amtyagi-ind <amtyagi@yahoo.com>
2026-06-14 15:55:20 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,6 +13,7 @@ a='Hello World'
 b="Also"
 d,e,f=1,2,3
 print(a+" "+b)
+fruits=["Apple","Banana","Cherry"]
 
 print(b)
 j=factorial(10)
@@ -20,3 +21,12 @@ j=factorial(10)
 print (j)
 print(d)
 print (fact(3,2,5))
+fruits[1],fruits[2]=fruits[2],fruits[1]
+x=5
+y=7
+x=x^y
+y=x^y
+x=x^y
+print(x)
+print(y)
+print (fruits[1])
